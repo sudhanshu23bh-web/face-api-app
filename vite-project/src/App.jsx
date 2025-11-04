c36939f8dfb915382ebcaad26cf693d758d31d1b
Sudhanshu Bhargava <sudhanshu23bh@gmail.com>
2025-11-04 20:53:26 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FacialExpression from "./components/facialExpression"
+
+function App() {
+  return (
+    <>
+      <FacialExpression />
+       
+    </>
+  )
+}
+
+export default App
